99ef59a9655c21bf7caa684028fdf24ae673840f
nowgnas <dev.nowgnas@gmail.com>
2023-11-19 03:10:24 +0900

    :tada: Start notification service

---
+++ src/test/java/kr/bb/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bb.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
