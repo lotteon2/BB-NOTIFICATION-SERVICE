72e27c6fe904cdc8b95689ec5ce8d2e13baaefb8
nowgnas <dev.nowgnas@gmail.com>
2023-11-21 20:51:52 +0900

    :card_file_box: Add notification entity

--- src/main/java/kr/bb/notification/NotificationServiceApplication.java
+++ src/main/java/kr/bb/notification/NotificationServiceApplication.java
@@ -3,13 +3,14 @@ package kr.bb.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaAuditing
 public class NotificationServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NotificationServiceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationServiceApplication.class, args);
+  }
 }
---
+++ src/main/java/kr/bb/notification/entity/MemberNotification.java
@@ -0,0 +1,43 @@
+package kr.bb.notification.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "member_notification")
+public class MemberNotification extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_notification_id")
+  private Long id;
+
+  @Column(name = "user_id")
+  private Long userId;
+
+  @Column(name = "is_read")
+  private Boolean isRead;
+
+  @ManyToOne(
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+  @JoinColumn(name = "notificationId")
+  private Notification notification;
+}
