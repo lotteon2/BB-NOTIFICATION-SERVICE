f2b3a1508e17e5e25391b38a976733f1d3c2749b
nowgnas <dev.nowgnas@gmail.com>
2023-12-03 02:00:23 +0900

    :adhesive_bandage: Client controller fix

--- src/main/java/kr/bb/notification/config/SseEmitters.java
+++
@@ -1,9 +0,0 @@
-package kr.bb.notification.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SseEmitters {
-
-}
