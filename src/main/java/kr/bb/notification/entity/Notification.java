3abac1dc22df5668984e8b2c1be3d89c4f94c3d5
nowgnas <dev.nowgnas@gmail.com>
2023-12-06 12:14:18 +0900

    :white_check_mark: 알림 저장 영속성 전이 테스트

--- src/main/java/kr/bb/notification/entity/Notification.java
+++ src/main/java/kr/bb/notification/entity/Notification.java
@@ -1,10 +1,15 @@
 package kr.bb.notification.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,4 +34,16 @@ public class Notification {
 
   @Column(name = "notification_link")
   private String notificationLink;
+
+  @OneToMany(
+      mappedBy = "notification",
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+      orphanRemoval = true)
+  @Builder.Default
+  private List<MemberNotification> memberNotifications = new ArrayList<>();
+
+  public void setMemberNotifications(List<MemberNotification> memberNotifications) {
+    this.memberNotifications = memberNotifications;
+  }
 }
--- src/test/java/kr/bb/notification/domain/notification/repository/NotificationJpaRepositoryTest.java
+++ src/test/java/kr/bb/notification/domain/notification/repository/NotificationJpaRepositoryTest.java
@@ -3,6 +3,7 @@ package kr.bb.notification.domain.notification.repository;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import kr.bb.notification.entity.MemberNotification;
 import kr.bb.notification.entity.Notification;
@@ -30,5 +31,25 @@ class NotificationJpaRepositoryTest {
     List<Notification> all = notificationJpaRepository.findAll();
     assertThat(all.size()).isEqualTo(1);
     assertThat(all.get(0).getNotificationLink()).isEqualTo(notiContent.getNotificationLink());
+  }
+
+  @Test
+  @DisplayName("알림 저장 테스트 영속성 전이")
+  void saveNotificationAndMemberNotification() {
+    List<MemberNotification> list = new ArrayList<>();
+    Notification content =
+        Notification.builder()
+            .notificationContent("content")
+            .notificationLink("/api/products/1")
+            .build();
+    for (int i = 0; i < 5; i++) {
+      MemberNotification build =
+          MemberNotification.builder().userId(1L).notification(content).build();
+      list.add(build);
+    }
+    content.setMemberNotifications(list);
+    notificationJpaRepository.save(content);
+    List<MemberNotification> all = memberNotificationJpaRepository.findAll();
+    assertThat(all.size()).isEqualTo(5);
   }
 }
